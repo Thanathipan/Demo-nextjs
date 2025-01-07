2749ab11da695e7a659df9a6b0c50500fc82f8ff
Thanathipan <thana.yy44@gmail.com>
2025-01-07 15:00:19 +0530

    add2

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI
-
+// MongoDB connection string
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const DBconnect = async () => {
     const connectionState = mongoose.connection.readyState;
@@ -11,19 +11,21 @@ const DBconnect = async () => {
         return;
     }
 
-    if ( connectionState === 2)
+    if (connectionState === 2) {
+        console.log('MongoDB connection is currently establishing');
+        return;
+    }
 
+    try {
+        await mongoose.connect(MONGODB_URI!, {
+            dbName: 'NextJSAPI',
+            bufferCommands: true,
+        });
+        console.log('MongoDB connection established successfully');
+    } catch (error: any) {
+        console.error('MongoDB connection error:', error.message);
+        throw new Error(`MongoDB connection error: ${error.message}`);
+    }
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-}
+export default DBconnect;

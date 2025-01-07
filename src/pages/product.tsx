4c88c46602eba4e2ad2f9653ba3cc74196d9a63d
Thanathipan <thana.yy44@gmail.com>
2025-01-07 12:29:28 +0530

    add

---
+++ src/pages/product.tsx
@@ -0,0 +1,15 @@
+import React , {Component} from 'react';
+export default class product extends Component {
+  render() {
+    return (
+      <div>
+        <h1>product</h1>
+      </div>
+    );
+  }
+}
+
+
+
+
+
